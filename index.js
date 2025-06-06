c12ae2a9d204f3e09bc0bf87f6c96ad7080d1cc7
mae-boittiaux <maeboittiaux@gmail.com>
2025-06-06 16:08:20 +0100

    feat(index.js): add all hex-codes to array from readme file no-duplicate list

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-const hexCodes = ['eccae4', '7e2567'];
+const hexCodes = [
+    '00000000',
+    '333333',
+    'a0a0a0',
+    'cecece',
+    'f0f0f0',
+    'fdfdfd',
+    'f8f8f2',
+    'f5e497',
+    '928135',
+    'eccae4',
+    '7e2567',
+    'ebc597',
+    'f2843e',
+    '855821',
+    'c7dcf0',
+    '6272a4',
+    '31597e',
+    '4b5575',
+    '343746',
+    '191a21',
+    'e0d2ec',
+    'bd76ad',
+    '340a57',
+    'd1ecba',
+    '214107',
+    'dd0534',
+    '9b1818',
+];
+
 const palette = document.getElementById('colour-palette');
 
 const addCSS = css => document.head.appendChild(document.createElement("style")).innerHTML = css;
