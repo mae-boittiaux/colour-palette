f33e177830a1b6fa9f6e3ba17475a17047b686d8
mae-boittiaux <maeboittiaux@gmail.com>
2025-06-06 12:22:18 +0100

    feat(palette): background of each colour in palette dynamically set to hex-code id

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const hexCodes = ['#eccae4', '#7e2567'];
+const hexCodes = ['eccae4', '7e2567'];
 const palette = document.getElementById('colour-palette');
+
+const addCSS = css => document.head.appendChild(document.createElement("style")).innerHTML = css;
 
 for (let i = 0; i < hexCodes.length; i++) {
     const colour = document.createElement('li');
 
-    colour.textContent = hexCodes[i];
-    colour.id = hexCodes[i];
+    colour.textContent = '#' + hexCodes[i];
+    colour.id = 'colour-' + hexCodes[i];
+
+    addCSS(`#${colour.id}{ background-color: #${hexCodes[i]}; }`);
 
     palette.appendChild(colour);
 }
